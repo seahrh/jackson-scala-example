77957ca52791297073b9a3bba851ca8f130486df
Ruhong <seahrh@gmail.com>
2018-06-19 07:19:11 +0800

    add and test deserialisation examples

--- src/main/scala/example/Root.scala
+++ src/main/scala/example/Root.scala
@@ -1,14 +1,27 @@
 package example
 
-import com.fasterxml.jackson.databind.{ObjectMapper, SerializationFeature}
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type
+import com.fasterxml.jackson.annotation.JsonTypeInfo.Id
+import com.fasterxml.jackson.annotation.{JsonSubTypes, JsonTypeInfo}
+import com.fasterxml.jackson.core.`type`.TypeReference
+import com.fasterxml.jackson.databind.{DeserializationFeature, ObjectMapper, SerializationFeature}
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
+import example.TRoot.mapper
 
+@JsonTypeInfo(use = Id.NAME,
+  include = JsonTypeInfo.As.PROPERTY,
+  property = "type")
+@JsonSubTypes(Array(
+  new Type(value = classOf[A]),
+  new Type(value = classOf[B]),
+  new Type(value = classOf[C])
+))
 sealed trait TRoot {
   val rootString: String
 
   def toJson: String = {
-    TRoot.toJson(this)
+    mapper.writeValueAsString(this)
   }
 }
 
@@ -16,11 +29,14 @@ object TRoot {
   private val mapper = new ObjectMapper() with ScalaObjectMapper
   mapper.registerModule(DefaultScalaModule)
   //mapper.registerModule(new JavaTimeModule())
-  //mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+  mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
   mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
 
-  def toJson(obj: TRoot): String = {
-    mapper.writeValueAsString(obj)
+  private[example] def fromJson[T <: TRoot](json: String): T = {
+    // Due to type erasure, need to provide
+    // an instance of TypeReference with all the needed Type information.
+    // @see https://stackoverflow.com/a/29824075/519951
+    mapper.readValue(json, new TypeReference[T] {})
   }
 }
 
@@ -36,13 +52,31 @@ final case class A(
                     override val rootString: String
                   ) extends TRoot
 
+object A {
+  def fromJson(json: String): A = {
+    TRoot.fromJson[A](json)
+  }
+}
+
 final case class B(
                     override val rootString: String,
                     override val l1String: String
                   ) extends TLevelOne
+
+object B {
+  def fromJson(json: String): B = {
+    TRoot.fromJson[B](json)
+  }
+}
 
 final case class C(
                     override val rootString: String,
                     override val l1String: String,
                     override val l2String: String
                   ) extends TLevelTwo
+
+object C {
+  def fromJson(json: String): C = {
+    TRoot.fromJson[C](json)
+  }
+}
--- src/test/scala/example/RootSpec.scala
+++ src/test/scala/example/RootSpec.scala
@@ -1,26 +1,47 @@
 package example
 
+import com.typesafe.scalalogging.LazyLogging
 import org.scalatest.FlatSpec
 
-class RootSpec extends FlatSpec {
+class RootSpec extends FlatSpec with LazyLogging {
   "Class A" should "serialize to json" in {
     val a: A = A("root=a")
+    val actual: String = a.toJson
+    logger.debug(s"actual=$actual")
     assertResult(
-      "{\"rootString\":\"root=a\"}"
-    )(a.toJson)
+      "{\"type\":\"A\",\"rootString\":\"root=a\"}"
+    )(actual)
+  }
+
+  it should "deserialize json" in {
+    val e: A = A("root=a")
+    val a: A = A.fromJson(e.toJson)
+    assertResult(e)(a)
   }
 
   "Class B" should "serialize to json" in {
     val b: B = B("root=b", "l1=b")
     assertResult(
-      "{\"rootString\":\"root=b\",\"l1String\":\"l1=b\"}"
+      "{\"type\":\"B\",\"rootString\":\"root=b\",\"l1String\":\"l1=b\"}"
     )(b.toJson)
+  }
+
+  it should "deserialize json" in {
+    val e: B = B("root=b", "l1=b")
+    val a: B = B.fromJson(e.toJson)
+    assertResult(e)(a)
   }
 
   "Class C" should "serialize to json" in {
     val c: C = C("root=c", "l1=c", "l2=c")
     assertResult(
-      "{\"rootString\":\"root=c\",\"l1String\":\"l1=c\",\"l2String\":\"l2=c\"}"
+      "{\"type\":\"C\",\"rootString\":\"root=c\",\"l1String\":\"l1=c\",\"l2String\":\"l2=c\"}"
     )(c.toJson)
+  }
+
+  it should "deserialize json" in {
+    val e: C = C("root=c", "l1=c", "l2=c")
+    val a: C = C.fromJson(e.toJson)
+    assertResult(e)(a)
   }
 }
