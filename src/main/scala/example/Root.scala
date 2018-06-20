0f1aab391f2561478d12314a6bd524fcc4145b6f
Ruhong <seahrh@gmail.com>
2018-06-20 21:44:02 +0800

    serde java.time.LocalDateTime in iso string format

--- src/main/scala/example/Root.scala
+++ src/main/scala/example/Root.scala
@@ -1,10 +1,13 @@
 package example
 
+import java.time.LocalDateTime
+
 import com.fasterxml.jackson.annotation.JsonSubTypes.Type
 import com.fasterxml.jackson.annotation.JsonTypeInfo.Id
 import com.fasterxml.jackson.annotation.{JsonSubTypes, JsonTypeInfo}
 import com.fasterxml.jackson.core.`type`.TypeReference
 import com.fasterxml.jackson.databind.{DeserializationFeature, ObjectMapper, SerializationFeature}
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
 import example.TRoot.mapper
@@ -19,6 +22,7 @@ import example.TRoot.mapper
 ))
 sealed trait TRoot {
   val rootString: String
+  val rootDateTime: LocalDateTime
 
   def toJson: String = {
     mapper.writeValueAsString(this)
@@ -28,9 +32,10 @@ sealed trait TRoot {
 object TRoot {
   private val mapper = new ObjectMapper() with ScalaObjectMapper
   mapper.registerModule(DefaultScalaModule)
-  //mapper.registerModule(new JavaTimeModule())
+  mapper.registerModule(new JavaTimeModule())
   mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
   mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
+  mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
 
   private[example] def fromJson[T <: TRoot](json: String): T = {
     // Due to type erasure, need to provide
@@ -49,7 +54,8 @@ sealed trait TLevelTwo extends TLevelOne {
 }
 
 final case class A(
-                    override val rootString: String
+                    override val rootString: String,
+                    override val rootDateTime: LocalDateTime
                   ) extends TRoot
 
 object A {
@@ -60,6 +66,7 @@ object A {
 
 final case class B(
                     override val rootString: String,
+                    override val rootDateTime: LocalDateTime,
                     override val l1String: String
                   ) extends TLevelOne
 
@@ -71,6 +78,7 @@ object B {
 
 final case class C(
                     override val rootString: String,
+                    override val rootDateTime: LocalDateTime,
                     override val l1String: String,
                     override val l2String: String
                   ) extends TLevelTwo
--- src/test/scala/example/RootSpec.scala
+++ src/test/scala/example/RootSpec.scala
@@ -1,46 +1,84 @@
 package example
 
+import java.time.LocalDateTime
+
 import com.typesafe.scalalogging.LazyLogging
 import org.scalatest.FlatSpec
 
 class RootSpec extends FlatSpec with LazyLogging {
   "Class A" should "serialize to json" in {
-    val a: A = A("root=a")
+    // datetime string to millisecond precision
+    val dts: String = "2018-01-01T14:59:59.999"
+    val a: A = A(
+      rootString = "root=a",
+      rootDateTime = LocalDateTime.parse(dts)
+    )
     val actual: String = a.toJson
     logger.debug(s"actual=$actual")
     assertResult(
-      "{\"type\":\"A\",\"rootString\":\"root=a\"}"
+      s"""{"type":"A","rootString":"root=a","rootDateTime":"$dts"}"""
     )(actual)
   }
 
   it should "deserialize json" in {
-    val e: A = A("root=a")
+    // datetime string to millisecond precision
+    val dts: String = "2018-01-01T14:59:59.999"
+    val e: A = A(
+      rootString = "root=a",
+      rootDateTime = LocalDateTime.parse(dts)
+    )
     val a: A = A.fromJson(e.toJson)
     assertResult(e)(a)
   }
 
   "Class B" should "serialize to json" in {
-    val b: B = B("root=b", "l1=b")
+    // datetime string to millisecond precision
+    val dts: String = "2018-01-01T14:59:59.999"
+    val b: B = B(
+      rootString = "root=b",
+      rootDateTime = LocalDateTime.parse(dts),
+      l1String = "l1=b"
+    )
     assertResult(
-      "{\"type\":\"B\",\"rootString\":\"root=b\",\"l1String\":\"l1=b\"}"
+      s"""{"type":"B","rootString":"root=b","rootDateTime":"$dts","l1String":"l1=b"}"""
     )(b.toJson)
   }
 
   it should "deserialize json" in {
-    val e: B = B("root=b", "l1=b")
+    // datetime string to millisecond precision
+    val dts: String = "2018-01-01T14:59:59.999"
+    val e: B = B(
+      rootString = "root=b",
+      rootDateTime = LocalDateTime.parse(dts),
+      l1String = "l1=b"
+    )
     val a: B = B.fromJson(e.toJson)
     assertResult(e)(a)
   }
 
   "Class C" should "serialize to json" in {
-    val c: C = C("root=c", "l1=c", "l2=c")
+    // datetime string to millisecond precision
+    val dts: String = "2018-01-01T14:59:59.999"
+    val c: C = C(
+      rootString = "root=c",
+      rootDateTime = LocalDateTime.parse(dts),
+      l1String = "l1=c",
+      l2String = "l2=c"
+    )
     assertResult(
-      "{\"type\":\"C\",\"rootString\":\"root=c\",\"l1String\":\"l1=c\",\"l2String\":\"l2=c\"}"
+      s"""{"type":"C","rootString":"root=c","rootDateTime":"$dts","l1String":"l1=c","l2String":"l2=c"}"""
     )(c.toJson)
   }
 
   it should "deserialize json" in {
-    val e: C = C("root=c", "l1=c", "l2=c")
+    // datetime string to millisecond precision
+    val dts: String = "2018-01-01T14:59:59.999"
+    val e: C = C(
+      rootString = "root=c",
+      rootDateTime = LocalDateTime.parse(dts),
+      l1String = "l1=c",
+      l2String = "l2=c"
+    )
     val a: C = C.fromJson(e.toJson)
     assertResult(e)(a)
   }
